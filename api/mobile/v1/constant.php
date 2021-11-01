23a6ddce6854603e0424b99b95670cfa83984ee1
arhanashik <ashik.pstu.cse@gmail.com>
2021-11-01 19:15:16 +0900

    fix : dev environment credentials

--- api/mobile/v1/constant.php
+++ api/mobile/v1/constant.php
@@ -2,9 +2,9 @@
 
 // ----------- v1 ----------//
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_NAME', 'pstuian_dev.db');
+define('DB_USER', 'pstuianc_dev');
+define('DB_PASS', 'qGPKUPvsCabp');
+define('DB_NAME', 'pstuianc_dev.db');
 
 // ----------- main tables -----------//
 define('SLIDER_TABLE', 'slider');
@@ -25,7 +25,7 @@ define('LOG_TABLE', 'log');
 define('NOTIFICATION_TABLE', 'notification');
 define('PASSWORD_RESET_TABLE', 'password_reset');
 
-define('BASE_URL', 'http://192.168.1.100:8888/PSTUian-web/');
+define('BASE_URL', 'https://dev.pstuian.com/');
 
 // Change the second parameter to suit your needs
 // Here 4 means, how many times it should go up
